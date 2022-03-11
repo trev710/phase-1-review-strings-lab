39c3a1a59b727a047d5222dc0f329a55288f1432
TrevorJohnson <trevor.assetsolutions@gmail.com>
2022-03-11 08:20:22 -0700

    assignment completed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const currentUser = 'Grace Hopper'; 
-const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`; 
+//set current user variable to 'Grace Hopper'
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
+//set a welcome message variable to concat a string to current user 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
+//use the touppercase method on the welcome message to make cases uppercase
 const shortGreeting = ` Welcome, ${currentUser.slice(0,1)}!`;
+//concat a greeting using the slice method on the current user string from the 0th index to the 1st
